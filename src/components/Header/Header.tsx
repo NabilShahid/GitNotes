e5f968d82f269a6dc01e58356c5f3ed3ecee4bd1
NabilShahid <nabil110176@gmail.com>
2020-05-14 03:13:58 -0700

    feat: style variables inclusion

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import { REDIRECT_URL } from '../../constants/github-app-info';
+import './Header.css';
 
 export interface HeaderProps {}
 
 const Header: React.SFC<HeaderProps> = () => (
-  <header>
+  <header className="main-header">
     <a href={REDIRECT_URL}>Login</a>
   </header>
 );
