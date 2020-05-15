d608303fb62d0869abf2574e59371fabacbfe110
NabilShahid <nabil110176@gmail.com>
2020-05-15 05:05:07 -0700

    feat: table view classes

--- src/components/PublicGists/PublicGists.tsx
+++ src/components/PublicGists/PublicGists.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
 import './PublicGists.css';
 import ICONS from '../../constants/icons';
+import Table from '../Table/Table';
 
 export interface PublicGistsProps {}
 
-const iconStyle = { width: '22px', height: '22px', fill: '#b4b4b4' };
+const iconStyle = { width: '22px', height: '22px', fill: '#d9d9d9' };
 const PublicGists: React.SFC<PublicGistsProps> = () => {
   return (
     <div className="public-gists-main-container">
-      <div className="text-align-right" style={{ margin: '22px 0px' }}>
+      <div className="text-align-right" style={{ margin: '20px 0px' }}>
         <div>
           <div className="public-gists-right-seperator">
             <ICONS.GridViewIcon style={iconStyle} />
@@ -17,6 +18,9 @@ const PublicGists: React.SFC<PublicGistsProps> = () => {
             <ICONS.ListViewIcon style={iconStyle} />
           </div>
         </div>
+      </div>
+      <div>
+        <Table />
       </div>
     </div>
   );
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,9 +1,66 @@
 import * as React from 'react';
+import './Table.css';
 
 export interface TableProps {}
 
 const Table: React.SFC<TableProps> = () => {
-  return <div>table</div>;
+  return (
+    <div className="table-container">
+      <div className="table-header">
+        <div className="table-header-row">
+          <div className="table-col">Name</div>
+          <div className="table-col">Date</div>
+          <div className="table-col">Time</div>
+          <div className="table-col">Keyword</div>
+          <div className="table-col">Notebook Name</div>
+        </div>
+      </div>
+      <div className="table-body">
+        <div className="table-body-row">
+          <div className="table-col">Name</div>
+          <div className="table-col">Date</div>
+          <div className="table-col">Time</div>
+          <div className="table-col">Keyword</div>
+          <div className="table-col">Notebook Name</div>
+        </div>
+        <div className="table-body-row">
+          <div className="table-col">Name</div>
+          <div className="table-col">Date</div>
+          <div className="table-col">Time</div>
+          <div className="table-col">Keyword</div>
+          <div className="table-col">Notebook Name</div>
+        </div>
+        <div className="table-body-row">
+          <div className="table-col">Name</div>
+          <div className="table-col">Date</div>
+          <div className="table-col">Time</div>
+          <div className="table-col">Keyword</div>
+          <div className="table-col">Notebook Name</div>
+        </div>
+        <div className="table-body-row">
+          <div className="table-col">Name</div>
+          <div className="table-col">Date</div>
+          <div className="table-col">Time</div>
+          <div className="table-col">Keyword</div>
+          <div className="table-col">Notebook Name</div>
+        </div>
+        <div className="table-body-row">
+          <div className="table-col">Name</div>
+          <div className="table-col">Date</div>
+          <div className="table-col">Time</div>
+          <div className="table-col">Keyword</div>
+          <div className="table-col">Notebook Name</div>
+        </div>
+        <div className="table-body-row">
+          <div className="table-col">Name</div>
+          <div className="table-col">Date</div>
+          <div className="table-col">Time</div>
+          <div className="table-col">Keyword</div>
+          <div className="table-col">Notebook Name</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Table;
