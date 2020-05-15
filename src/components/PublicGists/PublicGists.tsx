4a2f91aa8b569e8531bdac9257a51eeb0a1cb713
NabilShahid <nabil110176@gmail.com>
2020-05-15 04:32:43 -0700

    feat: grid list switcher

--- src/components/PublicGists/PublicGists.tsx
+++ src/components/PublicGists/PublicGists.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
+import './PublicGists.css';
+import ICONS from '../../constants/icons';
 
 export interface PublicGistsProps {}
 
+const iconStyle = { width: '22px', height: '22px', fill: '#b4b4b4' };
 const PublicGists: React.SFC<PublicGistsProps> = () => {
-  return <div className="public-gists-main-container">Public Gists</div>;
+  return (
+    <div className="public-gists-main-container">
+      <div className="text-align-right" style={{ margin: '22px 0px' }}>
+        <div>
+          <div className="public-gists-right-seperator">
+            <ICONS.GridViewIcon style={iconStyle} />
+          </div>
+          <div style={{ display: 'inline-block' }}>
+            <ICONS.ListViewIcon style={iconStyle} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PublicGists;
---
+++ src/constants/icons.ts
@@ -0,0 +1,9 @@
+import { ReactComponent as GridViewIcon } from '../assets/icons/design.svg';
+import { ReactComponent as ListViewIcon } from '../assets/icons/interface.svg';
+
+const ICONS = {
+  GridViewIcon,
+  ListViewIcon,
+};
+
+export default ICONS;
