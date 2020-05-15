83a0551cdb71c11b95c8374dd900db3985f27e64
NabilShahid <nabil110176@gmail.com>
2020-05-15 05:44:29 -0700

    feat: public gists show on table component

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import moment from 'moment';
 import './Table.css';
+import { useEffect, useState } from 'react';
+import { getPublicGists } from '../../services/apis';
+import UserAvatar from '../UserAvatar/UserAvatar';
 
 export interface TableProps {}
 
 const Table: React.SFC<TableProps> = () => {
+  const [gists, setGists]: [any, any] = useState([]);
+  useEffect(() => {
+    getPublicGists(1, 50).then((res: any) => {
+      setGists(res.data);
+    });
+  }, []);
   return (
     <div className="table-container">
       <div className="table-header">
@@ -16,48 +26,30 @@ const Table: React.SFC<TableProps> = () => {
         </div>
       </div>
       <div className="table-body">
-        <div className="table-body-row">
-          <div className="table-col">Name</div>
-          <div className="table-col">Date</div>
-          <div className="table-col">Time</div>
-          <div className="table-col">Keyword</div>
-          <div className="table-col">Notebook Name</div>
-        </div>
-        <div className="table-body-row">
-          <div className="table-col">Name</div>
-          <div className="table-col">Date</div>
-          <div className="table-col">Time</div>
-          <div className="table-col">Keyword</div>
-          <div className="table-col">Notebook Name</div>
-        </div>
-        <div className="table-body-row">
-          <div className="table-col">Name</div>
-          <div className="table-col">Date</div>
-          <div className="table-col">Time</div>
-          <div className="table-col">Keyword</div>
-          <div className="table-col">Notebook Name</div>
-        </div>
-        <div className="table-body-row">
-          <div className="table-col">Name</div>
-          <div className="table-col">Date</div>
-          <div className="table-col">Time</div>
-          <div className="table-col">Keyword</div>
-          <div className="table-col">Notebook Name</div>
-        </div>
-        <div className="table-body-row">
-          <div className="table-col">Name</div>
-          <div className="table-col">Date</div>
-          <div className="table-col">Time</div>
-          <div className="table-col">Keyword</div>
-          <div className="table-col">Notebook Name</div>
-        </div>
-        <div className="table-body-row">
-          <div className="table-col">Name</div>
-          <div className="table-col">Date</div>
-          <div className="table-col">Time</div>
-          <div className="table-col">Keyword</div>
-          <div className="table-col">Notebook Name</div>
-        </div>
+        {gists.length > 0 &&
+          gists.map((gist: any) => {
+            return (
+              <div className="table-body-row">
+                <div className="table-col">
+                  <div style={{ display: 'flex' }}>
+                    <div>
+                      <UserAvatar
+                        src={gist.owner.avatar_url}
+                        style={{ width: '30px', marginRight: '7px' }}
+                      />
+                    </div>
+                    <div style={{ marginTop: '4px' }}>{gist.owner.login}</div>
+                  </div>
+                </div>
+                <div className="table-col">
+                  {moment(gist.created_at).format('DD-MMM-YYYY')}
+                </div>
+                <div className="table-col">{moment(gist.created_at).format('HH:mm A')}</div>
+                <div className="table-col">{gist.description}</div>
+                <div className="table-col">{Object.keys(gist.files)[0]}</div>
+              </div>
+            );
+          })}
       </div>
     </div>
   );
