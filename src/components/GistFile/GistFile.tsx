b6bfc0bfb62721a6224f05f482673123731ec0de
NabilShahid <nabil110176@gmail.com>
2020-05-18 01:31:58 -0700

    chore: codemirror installation

---
+++ src/components/GistFile/GistFile.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { UnControlled as CodeMirror } from 'react-codemirror2';
+import './GitFile.css';
+
+export interface GistFileProps {}
+
+const GistFile: React.SFC<GistFileProps> = () => {
+  return (
+    <div className="gist-file-container">
+      <CodeMirror
+        value="<h1>I ♥ react-codemirror2</h1>"
+        options={{
+          mode: 'javascript',
+          theme: 'xq-light',
+          lineNumbers: true,
+        }}
+        onChange={(editor, data, value) => {}}
+      />
+    </div>
+  );
+};
+
+export default GistFile;
--- src/components/GistPage/GistPage.tsx
+++ src/components/GistPage/GistPage.tsx
@@ -1,17 +1,21 @@
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import GistInfo from '../GistInfo/GistInfo';
+import GistFile from '../GistFile/GistFile';
 
 export interface GistPageProps {}
 
 const GistPage: React.SFC<GistPageProps> = () => {
   const { id } = useParams();
   return (
-    <div>
-      User Gist:
-      {id}
-      <GistInfo />
-    </div>
+    <>
+      {/* <div>
+        User Gist:
+        {id}
+        <GistInfo />
+      </div> */}
+      <GistFile />
+    </>
   );
 };
 
