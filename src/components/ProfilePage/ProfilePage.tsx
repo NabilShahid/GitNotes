8ba40b796244b9facbe4c19d960111920fcea876
NabilShahid <nabil110176@gmail.com>
2020-05-19 03:03:41 -0700

    feat: user avatar on profile page

--- src/components/ProfilePage/ProfilePage.tsx
+++ src/components/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,34 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import './ProfilePage.css';
+import UserAvatar from '../UserAvatar/UserAvatar';
 
-export interface ProfilePageProps {}
+export interface ProfilePageProps {
+  avatarUrl?: string;
+  login?: string;
+}
 
-const ProfilePage: React.SFC<ProfilePageProps> = () => {
-  return <div>Profile Page</div>;
+const ProfilePage: React.SFC<ProfilePageProps> = ({
+  avatarUrl,
+  login,
+}: ProfilePageProps) => {
+  return (
+    <div className="profile-page-container">
+      <div className="profile-page-info-container">
+        <UserAvatar src={avatarUrl || ''} style={{ width: '220px' }} />
+        <div className="profile-page-login-name">{login}</div>
+      </div>
+    </div>
+  );
 };
 
-export default ProfilePage;
+/**
+ * state to props mapping
+ */
+const mapStateToProps = (state: any) => {
+  return {
+    avatarUrl: state.userReducer.User.AvatarUrl,
+    login: state.userReducer.User.Login,
+  };
+};
+export default connect(mapStateToProps, null)(ProfilePage);
