7e77dd46bc91971647cc79ea9cda4bcc083361f5
NabilShahid <nabil110176@gmail.com>
2020-05-20 16:04:55 +0500

    feat: simple popover show hide avatar click

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import Popover from 'react-popover';
 import { REDIRECT_URL } from '../../constants/github-app-info';
 import ActionButton from '../ActionButton/ActionButton';
 import './Header.css';
@@ -13,53 +14,75 @@ import ROUTES from '../../constants/routes';
 export interface HeaderProps {
   avatarUrl?: string;
 }
-
-const Header: React.SFC<HeaderProps> = ({ avatarUrl }: HeaderProps) => (
-  <header className="header material-box-shadow">
-    <div style={{ flex: '0 1 6%' }} />
-    <div
-      className="text-align-right cursor-pointer"
-      style={{ flex: '0 0 15%', marginTop: '5px' }}
-      onClick={() => {
-        history.push(ROUTES.Home);
-      }}
-      role="button"
-      onKeyPress={() => {}}
-      tabIndex={0}
-    >
-      <img src={eMumbaLogo} className="header-emumba-logo" alt="EMUMBA" />
-    </div>
-    <div style={{ flex: '0 1 45%' }} />
-    <div
-      className="text-align-right"
-      style={{ flex: '0 0 25%', marginTop: '3px' }}
-    >
-      <SearchBar input={() => {}} placeholder="Search Notes" />
-    </div>
-    <div
-      style={{
-        flex: '0 0 12%',
-        textAlign: 'left',
-        margin: '5px 0 0 22px',
-        height: 0,
-      }}
-    >
-      {getToken() ? (
-        <UserAvatar
-          src={avatarUrl || ''}
-          style={{ width: '40px', marginTop: '-4px' }}
-        />
-      ) : (
-        <ActionButton
-          click={() => {
-            window.location.href = REDIRECT_URL;
-          }}
-          text="Login"
-        />
-      )}
-    </div>
-  </header>
-);
+// const popoverProps = {
+//   isOpen: true,
+//   body: [<h1 key="a">Popover Title</h1>, <div key="b">Popover contents</div>],
+// };
+const Header: React.SFC<HeaderProps> = ({ avatarUrl }: HeaderProps) => {
+  const [popoverOpen, setPopoverOpen]: [boolean, Function] = React.useState(
+    false,
+  );
+  return (
+    <header className="header material-box-shadow">
+      <div style={{ flex: '0 1 6%' }} />
+      <div
+        className="text-align-right cursor-pointer"
+        style={{ flex: '0 0 15%', marginTop: '5px' }}
+        onClick={() => {
+          history.push(ROUTES.Home);
+        }}
+        role="button"
+        onKeyPress={() => {}}
+        tabIndex={0}
+      >
+        <img src={eMumbaLogo} className="header-emumba-logo" alt="EMUMBA" />
+      </div>
+      <div style={{ flex: '0 1 45%' }} />
+      <div
+        className="text-align-right"
+        style={{ flex: '0 0 25%', marginTop: '3px' }}
+      >
+        <SearchBar input={() => {}} placeholder="Search Notes" />
+      </div>
+      <div
+        style={{
+          flex: '0 0 12%',
+          textAlign: 'left',
+          margin: '5px 0 0 22px',
+          height: 0,
+        }}
+      >
+        {getToken() ? (
+          <Popover
+            isOpen={popoverOpen}
+            place="below"
+            body={<div style={{ background: 'white' }}>Hello</div>}
+          >
+            <button
+              onClick={() => {
+                setPopoverOpen(!popoverOpen);
+              }}
+              type="button"
+              className="transparent-button cursor-pointer"
+            >
+              <UserAvatar
+                src={avatarUrl || ''}
+                style={{ width: '40px', marginTop: '-4px' }}
+              />
+            </button>
+          </Popover>
+        ) : (
+          <ActionButton
+            click={() => {
+              window.location.href = REDIRECT_URL;
+            }}
+            text="Login"
+          />
+        )}
+      </div>
+    </header>
+  );
+};
 /**
  * state to props mapping
  */
