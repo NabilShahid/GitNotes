b1f02e6121a3a0e31eb1c1816c9f9aa8896f9a5d
NabilShahid <nabil110176@gmail.com>
2020-05-20 15:44:43 +0500

    fix: table cols widht

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -3,11 +3,18 @@ import moment from 'moment';
 import './Table.css';
 import UserAvatar from '../UserAvatar/UserAvatar';
 import history from '../../services/history';
+import ICONS from '../../constants/icons';
+import { forkGist, starGist } from '../../services/apis';
 
 export interface TableProps {
   gists: Array<any>;
 }
 
+const tableIconStyle = {
+  height: '15px',
+  width: '15px',
+  fill: 'var(--main-color)',
+};
 const Table: React.SFC<TableProps> = ({ gists }: TableProps) => {
   return (
     <div className="table-container">
@@ -15,9 +22,12 @@ const Table: React.SFC<TableProps> = ({ gists }: TableProps) => {
         <div className="table-header-row">
           <div className="table-col">Name</div>
           <div className="table-col">Date</div>
-          <div className="table-col">Time</div>
+          <div className="table-col" style={{ flexBasis: '10%' }}>
+            Time
+          </div>
           <div className="table-col">Keyword</div>
           <div className="table-col">Notebook Name</div>
+          <div className="table-col" style={{ flexBasis: '10%' }} />
         </div>
       </div>
       <div className="table-body">
@@ -47,11 +57,31 @@ const Table: React.SFC<TableProps> = ({ gists }: TableProps) => {
                 <div className="table-col">
                   {moment(gist.created_at).format('DD-MMM-YYYY')}
                 </div>
-                <div className="table-col">
+                <div className="table-col" style={{ flexBasis: '10%' }}>
                   {moment(gist.created_at).format('HH:mm A')}
                 </div>
                 <div className="table-col">{gist.description}</div>
                 <div className="table-col">{Object.keys(gist.files)[0]}</div>
+                <div
+                  className="table-col"
+                  style={{ textAlign: 'center', flexBasis: '10%' }}
+                >
+                  {' '}
+                  <ICONS.StarIcon
+                    onClick={() => {
+                      forkGist(gist.id).then(() => {
+                        // setForksCount(forksCount + 1);
+                      });
+                    }}
+                    style={tableIconStyle}
+                  />
+                  <ICONS.ForkIcon
+                    onClick={() => {
+                      starGist(gist.id).then(() => {});
+                    }}
+                    style={{ ...tableIconStyle, marginLeft: '20px' }}
+                  />
+                </div>
               </div>
             );
           })}
